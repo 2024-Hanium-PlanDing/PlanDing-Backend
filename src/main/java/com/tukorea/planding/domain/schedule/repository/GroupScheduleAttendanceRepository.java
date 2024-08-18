d6c86bbd734a639b6b53f54b97a334068ad4d579
SangWoon123 <alrax3574@naver.com>
2024-08-18 23:55:42 +0900

    fix: 그룹 스케줄 참여

--- src/main/java/com/tukorea/planding/domain/schedule/repository/GroupScheduleAttendanceRepository.java
+++ src/main/java/com/tukorea/planding/domain/schedule/repository/GroupScheduleAttendanceRepository.java
@@ -1,6 +1,7 @@
 package com.tukorea.planding.domain.schedule.repository;
 
 import com.tukorea.planding.domain.schedule.entity.GroupScheduleAttendance;
+import com.tukorea.planding.domain.schedule.entity.Schedule;
 import com.tukorea.planding.domain.schedule.entity.ScheduleStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,4 +10,8 @@ import java.util.Optional;
 public interface GroupScheduleAttendanceRepository extends JpaRepository<GroupScheduleAttendance, Long> {
     Optional<GroupScheduleAttendance> findByUserIdAndScheduleIdAndStatusNot(Long userId, Long scheduleId, ScheduleStatus status);
     Optional<GroupScheduleAttendance> findByUserIdAndScheduleId(Long userId, Long scheduleId);
+    Optional<GroupScheduleAttendance> findByUser_UserCodeAndSchedule_IdAndStatus(String userCode, Long scheduleId, ScheduleStatus status);
+
+    void deleteBySchedule(Schedule schedule);
+
 }
