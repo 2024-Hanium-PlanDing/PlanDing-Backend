08ba40db6317a045f61ce8a24d93065abd3258da
SangWoon123 <alrax3574@naver.com>
2024-09-29 00:29:59 +0900

    fix: 애플리케이션 캐싱설정

--- src/main/java/com/tukorea/planding/PlandingApplication.java
+++ src/main/java/com/tukorea/planding/PlandingApplication.java
@@ -2,16 +2,18 @@ package com.tukorea.planding;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
+@EnableCaching
 public class PlandingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PlandingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PlandingApplication.class, args);
+    }
 
 }
--- src/main/java/com/tukorea/planding/domain/schedule/controller/CommonScheduleController.java
+++ src/main/java/com/tukorea/planding/domain/schedule/controller/CommonScheduleController.java
@@ -2,8 +2,6 @@ package com.tukorea.planding.domain.schedule.controller;
 
 import com.tukorea.planding.common.CommonResponse;
 import com.tukorea.planding.common.CommonUtils;
-import com.tukorea.planding.domain.schedule.dto.request.ScheduleRequest;
-import com.tukorea.planding.domain.schedule.dto.response.PersonalScheduleResponse;
 import com.tukorea.planding.domain.schedule.dto.response.ScheduleResponse;
 import com.tukorea.planding.domain.schedule.service.CommonScheduleService;
 import com.tukorea.planding.domain.user.dto.UserInfo;
@@ -11,7 +9,10 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 import java.util.List;
