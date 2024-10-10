44d3cf145be50e9cec48f6643930d1075ce61370
SangWoon123 <alrax3574@naver.com>
2024-10-10 14:46:53 +0900

    fix: oauth핸들러수정

--- src/main/java/com/tukorea/planding/global/oauth/handler/Oauth2SuccessHandler.java
+++ src/main/java/com/tukorea/planding/global/oauth/handler/Oauth2SuccessHandler.java
@@ -1,12 +1,8 @@
 package com.tukorea.planding.global.oauth.handler;
 
-import com.tukorea.planding.domain.auth.dto.TemporaryTokenRequest;
 import com.tukorea.planding.domain.auth.dto.TemporaryTokenResponse;
-import com.tukorea.planding.domain.auth.dto.TokenResponse;
-import com.tukorea.planding.domain.auth.repository.TokenInfoCacheRepository;
 import com.tukorea.planding.domain.auth.service.TokenService;
 import com.tukorea.planding.global.config.security.jwt.JwtProperties;
-import com.tukorea.planding.global.config.security.jwt.JwtUtil;
 import com.tukorea.planding.global.oauth.service.CustomOAuth2User;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -25,10 +21,8 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class Oauth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
-    private final JwtUtil jwtUtil;
     private final TokenService tokenService;
     private final JwtProperties jwtProperties;
-    private final TokenInfoCacheRepository tokenInfoCacheRepository;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
@@ -36,16 +30,9 @@ public class Oauth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
 
         CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
 
-
-        TemporaryTokenResponse temporaryToken=tokenService.createTemporaryTokenResponse(oAuth2User.getUserId(), oAuth2User.getUserCode());
-//        TokenResponse newToken = tokenService.createNewToken(oAuth2User.getUserId(), oAuth2User.getUserCode());
+        TemporaryTokenResponse temporaryToken = tokenService.createTemporaryTokenResponse(oAuth2User.getUserId(), oAuth2User.getUserCode());
 
         log.info("임시token생성 ={}", temporaryToken.temporaryToken());
-//        log.info("token생성 ={}", newToken.accessToken());
-//        log.info("refresh생성 ={}", newToken.refreshToken());
-
-        //쿠키 반환
-//        jwtUtil.setTokensInResponse(response, newToken.accessToken(), newToken.refreshToken());
 
         String url = makeRedirectUrl(temporaryToken.temporaryToken());
         getRedirectStrategy().sendRedirect(request, response, url);
