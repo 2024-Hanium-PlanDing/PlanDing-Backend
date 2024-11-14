71e2271fdec0a92f7ce7e9dbdb8ef5d488f4809e
SangWoon123 <alrax3574@naver.com>
2024-11-14 15:05:29 +0900

    refactor: GroupFavorite 엔티티/도메인 분리

--- src/main/java/com/tukorea/planding/domain/group/entity/GroupFavorite.java
+++ src/main/java/com/tukorea/planding/domain/group/entity/GroupFavorite.java
@@ -1,8 +1,11 @@
 package com.tukorea.planding.domain.group.entity;
 
+import com.tukorea.planding.domain.group.entity.domain.GroupFavoriteDomain;
+import com.tukorea.planding.domain.group.entity.domain.GroupRoomDomain;
 import com.tukorea.planding.domain.user.entity.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -23,12 +26,26 @@ public class GroupFavorite {
     @JoinColumn(name = "group_id")
     private GroupRoom groupRoom;
 
-    private GroupFavorite(User user, GroupRoom groupRoom) {
+    @Builder
+    public GroupFavorite(Long id, User user, GroupRoom groupRoom) {
+        this.id = id;
         this.user = user;
         this.groupRoom = groupRoom;
     }
 
-    public static GroupFavorite createGroupFavorite(User user, GroupRoom groupRoom) {
-        return new GroupFavorite(user, groupRoom);
+    public GroupFavoriteDomain toModel() {
+        return GroupFavoriteDomain.builder()
+                .id(id)
+                .groupRoomDomain(groupRoom.toModel())
+                .userDomain(user.toModel())
+                .build();
+    }
+
+    public static GroupFavorite fromModel(GroupFavoriteDomain groupFavoriteDomain) {
+        return GroupFavorite.builder()
+                .id(groupFavoriteDomain.getId())
+                .groupRoom(GroupRoom.fromModel(groupFavoriteDomain.getGroupRoomDomain()))
+                .user(User.fromModel(groupFavoriteDomain.getUserDomain()))
+                .build();
     }
 }
--- src/main/java/com/tukorea/planding/domain/group/entity/domain/GroupFavoriteDomain.java
+++ src/main/java/com/tukorea/planding/domain/group/entity/domain/GroupFavoriteDomain.java
@@ -1,15 +1,27 @@
 package com.tukorea.planding.domain.group.entity.domain;
 
-import com.tukorea.planding.domain.group.entity.GroupRoom;
-import com.tukorea.planding.domain.user.entity.User;
 import com.tukorea.planding.domain.user.entity.UserDomain;
+import lombok.Builder;
+import lombok.Getter;
 
+@Getter
 public class GroupFavoriteDomain {
 
-        private Long id;
+    private final Long id;
+    private final UserDomain userDomain;
+    private final GroupRoomDomain groupRoomDomain;
 
-        private UserDomain user;
+    @Builder
+    public GroupFavoriteDomain(Long id, UserDomain userDomain, GroupRoomDomain groupRoomDomain) {
+        this.id = id;
+        this.userDomain = userDomain;
+        this.groupRoomDomain = groupRoomDomain;
+    }
 
-        private GroupRoomDomain groupRoom;
-
+    public static GroupFavoriteDomain createGroupFavorite(UserDomain userDomain, GroupRoomDomain groupRoomDomain) {
+        return GroupFavoriteDomain.builder()
+                .userDomain(userDomain)
+                .groupRoomDomain(groupRoomDomain)
+                .build();
+    }
 }
